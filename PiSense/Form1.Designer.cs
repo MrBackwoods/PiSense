d9ceba93413b7ec15d7dd9362b176d6b411542df
JoniRokkanen <j.rokkanen@gmail.com>
2018-06-26 11:22:45 +0300

    First commit

---
+++ PiSense/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PiSense
+{
+    partial class PiS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TemperatureBox = new System.Windows.Forms.TextBox();
+            this.HumidityBox = new System.Windows.Forms.TextBox();
+            this.PiSenseLabel = new System.Windows.Forms.Label();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TemperatureBox
+            // 
+            this.TemperatureBox.BackColor = System.Drawing.Color.Aqua;
+            this.TemperatureBox.Location = new System.Drawing.Point(0, 46);
+            this.TemperatureBox.Name = "TemperatureBox";
+            this.TemperatureBox.ReadOnly = true;
+            this.TemperatureBox.Size = new System.Drawing.Size(233, 20);
+            this.TemperatureBox.TabIndex = 0;
+            this.TemperatureBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // HumidityBox
+            // 
+            this.HumidityBox.BackColor = System.Drawing.Color.GreenYellow;
+            this.HumidityBox.Location = new System.Drawing.Point(0, 72);
+            this.HumidityBox.Name = "HumidityBox";
+            this.HumidityBox.ReadOnly = true;
+            this.HumidityBox.Size = new System.Drawing.Size(233, 20);
+            this.HumidityBox.TabIndex = 1;
+            this.HumidityBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PiSenseLabel
+            // 
+            this.PiSenseLabel.AutoSize = true;
+            this.PiSenseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PiSenseLabel.Location = new System.Drawing.Point(36, 9);
+            this.PiSenseLabel.Margin = new System.Windows.Forms.Padding(100, 0, 3, 500);
+            this.PiSenseLabel.Name = "PiSenseLabel";
+            this.PiSenseLabel.Size = new System.Drawing.Size(160, 31);
+            this.PiSenseLabel.TabIndex = 2;
+            this.PiSenseLabel.Text = "PiSense 1.0";
+            this.PiSenseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(13, 108);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(208, 113);
+            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox.TabIndex = 3;
+            this.PictureBox.TabStop = false;
+            // 
+            // PiS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 233);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.PiSenseLabel);
+            this.Controls.Add(this.HumidityBox);
+            this.Controls.Add(this.TemperatureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "PiS";
+            this.Text = "PiSense";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TemperatureBox;
+        private System.Windows.Forms.TextBox HumidityBox;
+        private System.Windows.Forms.Label PiSenseLabel;
+        private System.Windows.Forms.PictureBox PictureBox;
+    }
+}
+
---
+++ PiSense/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Web.Script.Serialization;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace PiSense
+{
+    public partial class PiS : Form
+    {
+        public PiS()
+        {
+            InitializeComponent();
+        }
+
+        // Variables to store sensor and webcam data
+        public SensorData data;
+        public Image image;
+
+        // Raspberry PI's API's user credentials
+        public string username = "Test";
+        public string password = "Pass";
+
+        // Starts updating data to form at form load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetPiData();
+        }
+
+        // Gets new data from Raspberry Pi every 10 seconds
+        void GetPiData()
+        {
+            try
+            {
+                var client = new WebClient();
+                client.Encoding = Encoding.UTF8;
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+                client.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
+
+                var timer = new System.Threading.Timer((e) =>
+                {
+                    Stream imgStream = client.OpenRead("http://10.0.0.2:8001/webcam");
+                    string sensorJson = client.DownloadString("http://10.0.0.2:8001/hometemp");
+                    data = serializer.Deserialize<SensorData>(sensorJson);
+                    image = Image.FromStream(imgStream);
+
+                    if (InvokeRequired)
+                    {
+                        Invoke(new MethodInvoker(SetFormData));
+                    }
+                    else
+                    {
+                        SetFormData();
+                    }
+
+                }, null, TimeSpan.Zero, TimeSpan.FromSeconds(30));
+            }
+            catch (Exception e)
+            {
+                EventLog.WriteEntry("PiSense", e.Message + "Trace" + e.StackTrace, EventLogEntryType.Error, 121, short.MaxValue);
+                Application.Exit();
+            }
+        }
+
+        // Updates form with new data
+        void SetFormData()
+        {
+            TemperatureBox.Text = "Temparature: " + data.Temperature.ToString();
+            HumidityBox.Text =  "Humidity: " + data.Humidity.ToString();
+            PictureBox.Image = image;
+        }
+
+        // Helper class to contain temperature and humidity
+        public class SensorData
+        {
+            public string Temperature
+            {
+                get; set;
+            }
+
+            public string Humidity
+            {
+                get; set;
+            }
+        }
+    }
+}
